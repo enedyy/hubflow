60d12b4cfe747968012ca9b5f5165e0d482961ec
JoaoVic-ribeiro <127441013+JoaoVic-ribeiro@users.noreply.github.com>
2024-12-05 21:03:46 -0300

    só subindo

--- index_Make/src/pages/cadastro/Informacoes.jsx
+++ index_Make/src/pages/cadastro/Informacoes.jsx
@@ -1,79 +1,99 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { authCadastro } from "@/context/authCadastro";
-import { authLogin } from "@/context/authLogin";
-import { authProtecao_Rotas } from "@/context/authProtecao_rotas";
-import axios from "axios";
+  import React, { useState } from "react";
+  import { useNavigate } from "react-router-dom";
+  import { authCadastro } from "@/context/authCadastro";
+  import { authLogin } from "@/context/authLogin";
+  import { authProtecao_Rotas } from "@/context/authProtecao_rotas";
+  import axios from "axios";
 
-// -------- COMPONENTES UI
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
-import { Input } from "@/components/ui/input";
-import CircularProgress from "@mui/material/CircularProgress";
-import { motion } from "framer-motion";
+  // -------- COMPONENTES UI
+  import { Button } from "@/components/ui/button";
+  import { Label } from "@/components/ui/label";
+  import { Separator } from "@/components/ui/separator";
+  import { Input } from "@/components/ui/input";
+  import CircularProgress from "@mui/material/CircularProgress";
+  import { motion } from "framer-motion";
 
-const Informacoes = () => {
-  const [btnLoading_Submit, set_btnLoading_Submit] = useState(false);
-  const [isSenha, set_isSenha] = useState(true);
-  const navigate = useNavigate();
-  const { resetEtapa, etapa } = authProtecao_Rotas();
-  const {
-          email,
-          nome,
-          tel,
-          dataNascimento,
+  const Informacoes = () => {
+    const [btnLoading_Submit, set_btnLoading_Submit] = useState(false);
+    const [isSenha, set_isSenha] = useState(true);
+    const navigate = useNavigate();
+    const { resetEtapa, etapa } = authProtecao_Rotas();
+    const {
+            email,
+            nome,
+            tel,
+            dataNascimento,
 
-          cep, // verificar isso.
+            cep, // verificar isso.
 
-          numero,
-          senha,
-          empresa,
-          user,
-          estado,
-          cidade,
-          bairro,
-          rua,
-          cpf,
-          cnpj
-  } = authCadastro();
+            numero,
+            senha,
+            empresa,
+            user,
+            estado,
+            cidade,
+            bairro,
+            rua,
+            cpf,
+            cnpj
+    } = authCadastro();
 
-  // Mostrar/Ocultar senha
-  const mostrarSenha = () => set_isSenha((prev) => !prev);
+    // Mostrar/Ocultar senha
+    const mostrarSenha = () => set_isSenha((prev) => !prev);
 
-  // Função para limpar os dados do cadastro
-  const limparDadosCadastro = () => {
-    const campos = [
-    "email",
-    "nome",
-    "tel",
-    "dataNascimento",
-    "cep",
-    "numero",
-    "senha",
-    "empresa",
-    "user",
-    "estado",
-    "cidade",
-    "bairro",
-    "rua",
-    "cpf",
-    "cnpj"
-    ];
+    // Função para limpar os dados do cadastro
+    const limparDadosCadastro = () => {
+      const campos = [
+      "email",
+      "nome",
+      "tel",
+      "dataNascimento",
+      "cep",
+      "numero",
+      "senha",
+      "empresa",
+      "user",
+      "estado",
+      "cidade",
+      "bairro",
+      "rua",
+      "cpf",
+      "cnpj"
+      ];
 
-    campos.forEach((campo) => {
-      authCadastro.getState().removeUserInfo(campo);
-    });
-  };
+      campos.forEach((campo) => {
+        authCadastro.getState().removeUserInfo(campo);
+      });
+    };
 
-  // Função de envio do formulário
+    // Função de envio do formulário
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    set_btnLoading_Submit(true);
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      set_btnLoading_Submit(true);
 
-    try {
-        console.log("Enviando dados:", {
+      try {
+          console.log("Enviando dados:", {
+            email,
+            nome,
+            tel,
+            dataNascimento,
+            cep,
+            numero,
+            senha,
+            empresa,
+            user,
+            estado,
+            cidade,
+            bairro,
+            rua,
+            cpf,
+            cnpj
+          });
+
+          // const response = await axios.post('http://localhost/tcc2/tcc_Make/hubflow/Backend/api/cadastro.php', 
+            const response = await axios.post('http://172.20.10.5/hub/hubflow/hubflow/api_vazia/users', 
+        JSON.stringify({
           email,
           nome,
           tel,
@@ -87,145 +107,134 @@ const Informacoes = () => {
           cidade,
           bairro,
           rua,
-          cpf,
+          cpf: null,
           cnpj
-        });
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
+        }
+    );
+    
+          console.log("Resposta do servidor:", response.data);
 
-        // const response = await axios.post('http://localhost/tcc2/tcc_Make/hubflow/Backend/api/cadastro.php', 
-          const response = await axios.post('http://192.168.1.216/hub/hubflow/hubflow/api_vazia/users', 
-      JSON.stringify({
-        email,
-        nome,
-        tel,
-        dataNascimento,
-        cep,
-        numero,
-        senha,
-        empresa,
-        user,
-        estado,
-        cidade,
-        bairro,
-        rua,
-        cpf: null,
-        cnpj
-      }),
-      {
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          withCredentials: true
+          authLogin.getState().login({
+            id: user === "cliente" ? response.data.clienteId : response.data.empresaId,
+            email,
+            nome,
+            tipoUser: user,
+          });
+
+        navigate("/");
+        // resetEtapa();
+      } catch (err) {
+        console.error("Erro ao criar usuário:", err); 
+        if (err.response) {
+          console.error("Erro do servidor:", err.response.data);
+        }
+        navigate("../../erro");
+      } finally {
+        // resetEtapa();
+        limparDadosCadastro();
+        set_btnLoading_Submit(false);
       }
-  );
+    };
 
-        console.log("Resposta do servidor:", response.data);
-
-      // authLogin.getState().login({
-      //   id:
-      //     user === "cliente"
-      //       ? response.data.clienteId
-      //       : response.data.empresaId,
-      //   email,
-      //   nome,
-      //   tipoUser: user,
-      // });
-
-      navigate("/");
-      // resetEtapa();
-    } catch (err) {
-      console.error("Erro ao criar usuário:", err); 
-      if (err.response) {
-        console.error("Erro do servidor:", err.response.data);
-      }
-      navigate("../../erro");
-    } finally {
-      // resetEtapa();
-      limparDadosCadastro();
-      set_btnLoading_Submit(false);
-    }
-  };
-
-  return (
-    <motion.div
-      initial={{ opacity: 0, x: 100 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -100 }}
-      transition={{ duration: 0.3 }}
-      className="h-full"
-    >
-      <form className="w-full h-full flex flex-col justify-center items-center gap-6 px-4">
-        <section className="flex flex-col w-3/4 gap-3">
-          <Label size="subtitle">Suas informações</Label>
-          <section className="flex flex-col gap-1 rounded-sm p-2.5">
-            <div className="flex flex-wrap gap-x-8 gap-y-2.5">
-              <section className="flex items-center w-full gap-2">
-                <Label size="base" color="colorText_Bold">
-                  Email:
-                </Label>
-                <Input className="flex-grow" value={email || "null"} readOnly />
-              </section>
-
-              <section className="flex items-center w-full gap-2">
-                <Label size="base" color="colorText_Bold">
-                  Nome:
-                </Label>
-                <Input className="flex-grow" value={nome || "null"} readOnly />
-              </section>
-
-              {empresa && (
+    return (
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.3 }}
+        className="h-full"
+      >
+        <form className="w-full h-full flex flex-col justify-center items-center gap-6 px-4">
+          <section className="flex flex-col w-3/4 gap-3">
+            <Label size="subtitle">Suas informações</Label>
+            <section className="flex flex-col gap-1 rounded-sm p-2.5">
+              <div className="flex flex-wrap gap-x-8 gap-y-2.5">
                 <section className="flex items-center w-full gap-2">
                   <Label size="base" color="colorText_Bold">
-                    Empresa:
+                    Email:
                   </Label>
-                  <Input
-                    className="flex-grow"
-                    value={empresa || "null"}
-                    readOnly
-                  />
+                  <Input className="flex-grow" value={email || "null"} readOnly />
                 </section>
-              )}
 
-              <section className="flex items-center w-full gap-2">
-                <Label size="base" color="colorText_Bold">
-                  Telefone:
-                </Label>
-                <Input className="flex-grow" value={tel || "null"} readOnly />
-              </section>
-            </div>
-
-            {/* Separador de Endereço */}
-            <div className="w-32 my-2">
-              <Label size="large">Endereço</Label>
-              <Separator />
-            </div>
-
-            {/* Informações de Endereço */}
-            <div className="flex flex-wrap gap-x-8 gap-y-2.5">
-              {/* CEP */}
-              <section className="flex items-center w-2/4 max-sm:w-full gap-2">
-                <Label size="base" color="colorText_Bold">
-                  CEP:
-                </Label>
-                <Input
-                  className="flex-grow"
-                  value={cep || "Não disponível"}
-                  readOnly
-                />
-              </section>
-
-              {/* Estado e Cidade */}
-              <div className="w-full flex gap-8">
-                <section className="flex items-center min-w-1/4 max-sm:w-full gap-2">
+                <section className="flex items-center w-full gap-2">
                   <Label size="base" color="colorText_Bold">
-                    Estado:
+                    Nome:
+                  </Label>
+                  <Input className="flex-grow" value={nome || "null"} readOnly />
+                </section>
+
+                {empresa && (
+                  <section className="flex items-center w-full gap-2">
+                    <Label size="base" color="colorText_Bold">
+                      Empresa:
+                    </Label>
+                    <Input
+                      className="flex-grow"
+                      value={empresa || "null"}
+                      readOnly
+                    />
+                  </section>
+                )}
+
+                <section className="flex items-center w-full gap-2">
+                  <Label size="base" color="colorText_Bold">
+                    Telefone:
+                  </Label>
+                  <Input className="flex-grow" value={tel || "null"} readOnly />
+                </section>
+              </div>
+
+              {/* Separador de Endereço */}
+              <div className="w-32 my-2">
+                <Label size="large">Endereço</Label>
+                <Separator />
+              </div>
+
+              {/* Informações de Endereço */}
+              <div className="flex flex-wrap gap-x-8 gap-y-2.5">
+                {/* CEP */}
+                <section className="flex items-center w-2/4 max-sm:w-full gap-2">
+                  <Label size="base" color="colorText_Bold">
+                    CEP:
                   </Label>
                   <Input
                     className="flex-grow"
-                    value={estado || "Não disponível"}
+                    value={cep || "Não disponível"}
                     readOnly
                   />
                 </section>
-                <section className="flex items-center w-3/4 max-sm:hidden gap-2">
+
+                {/* Estado e Cidade */}
+                <div className="w-full flex gap-8">
+                  <section className="flex items-center min-w-1/4 max-sm:w-full gap-2">
+                    <Label size="base" color="colorText_Bold">
+                      Estado:
+                    </Label>
+                    <Input
+                      className="flex-grow"
+                      value={estado || "Não disponível"}
+                      readOnly
+                    />
+                  </section>
+                  <section className="flex items-center w-3/4 max-sm:hidden gap-2">
+                    <Label size="base" color="colorText_Bold">
+                      Cidade:
+                    </Label>
+                    <Input
+                      className="flex-grow"
+                      value={cidade || "Não disponível"}
+                      readOnly
+                    />
+                  </section>
+                </div>
+
+                <section className="flex items-center w-full sm:hidden gap-2">
                   <Label size="base" color="colorText_Bold">
                     Cidade:
                   </Label>
@@ -235,44 +244,44 @@ const Informacoes = () => {
                     readOnly
                   />
                 </section>
-              </div>
 
-              <section className="flex items-center w-full sm:hidden gap-2">
-                <Label size="base" color="colorText_Bold">
-                  Cidade:
-                </Label>
-                <Input
-                  className="flex-grow"
-                  value={cidade || "Não disponível"}
-                  readOnly
-                />
-              </section>
-
-              {/* Bairro */}
-              <section className="flex items-center w-full gap-2">
-                <Label size="base" color="colorText_Bold">
-                  Bairro:
-                </Label>
-                <Input
-                  className="flex-grow"
-                  value={bairro || "Não disponível"}
-                  readOnly
-                />
-              </section>
-
-              {/* Rua e Número */}
-              <div className="w-full flex gap-2">
-                <section className="flex items-center w-3/4 max-sm:w-full gap-2">
+                {/* Bairro */}
+                <section className="flex items-center w-full gap-2">
                   <Label size="base" color="colorText_Bold">
-                    Rua:
+                    Bairro:
                   </Label>
                   <Input
                     className="flex-grow"
-                    value={rua || "Não disponível"}
+                    value={bairro || "Não disponível"}
                     readOnly
                   />
                 </section>
-                <section className="flex items-center w-2/4 max-sm:hidden gap-2">
+
+                {/* Rua e Número */}
+                <div className="w-full flex gap-2">
+                  <section className="flex items-center w-3/4 max-sm:w-full gap-2">
+                    <Label size="base" color="colorText_Bold">
+                      Rua:
+                    </Label>
+                    <Input
+                      className="flex-grow"
+                      value={rua || "Não disponível"}
+                      readOnly
+                    />
+                  </section>
+                  <section className="flex items-center w-2/4 max-sm:hidden gap-2">
+                    <Label size="base" color="colorText_Bold">
+                      Número:
+                    </Label>
+                    <Input
+                      className="flex-grow"
+                      value={numero || "Não disponível"}
+                      readOnly
+                    />
+                  </section>
+                </div>
+
+                <section className="flex items-center w-2/4 sm:hidden gap-2">
                   <Label size="base" color="colorText_Bold">
                     Número:
                   </Label>
@@ -283,42 +292,30 @@ const Informacoes = () => {
                   />
                 </section>
               </div>
+            </section>
 
-              <section className="flex items-center w-2/4 sm:hidden gap-2">
-                <Label size="base" color="colorText_Bold">
-                  Número:
-                </Label>
-                <Input
-                  className="flex-grow"
-                  value={numero || "Não disponível"}
-                  readOnly
+            {/* Botão de Cadastro */}
+            <Button
+              variant="primary"
+              className="relative flex items-center justify-center mt-2"
+              type="submit"
+              onClick={onSubmit}
+              disabled={btnLoading_Submit}
+            >
+              {btnLoading_Submit ? (
+                <CircularProgress
+                  size={20}
+                  color="colorPrimary"
+                  className="relative inset-0 mt-1"
                 />
-              </section>
-            </div>
+              ) : (
+                "Cadastrar"
+              )}
+            </Button>
           </section>
+        </form>
+      </motion.div>
+    );
+  };
 
-          {/* Botão de Cadastro */}
-          <Button
-            variant="primary"
-            className="relative flex items-center justify-center mt-2"
-            type="submit"
-            onClick={onSubmit}
-            disabled={btnLoading_Submit}
-          >
-            {btnLoading_Submit ? (
-              <CircularProgress
-                size={20}
-                color="colorPrimary"
-                className="relative inset-0 mt-1"
-              />
-            ) : (
-              "Cadastrar"
-            )}
-          </Button>
-        </section>
-      </form>
-    </motion.div>
-  );
-};
-
-export default Informacoes;
+  export default Informacoes;

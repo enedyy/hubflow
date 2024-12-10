7055a7e86b9c7549811e91e29dae8ba1e3c62dbe
enedyy <enedynery71@gmail.com>
2024-12-10 17:56:21 -0300

    a extensao foi modificada de js para cjs por causa da compatibilidade do nodejs

--- index_Make/tailwind.config.cjs
+++ index_Make/tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -74,4 +74,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}

84a529a372c27b45d89a9f53441fd85f179e48d3
Eduardo Machado <eduardo.silvamachado07@gmail.com>
2024-12-10 13:45:45 -0300

    contexto de login atualizado

--- index_Make/src/pages/navbar.jsx
+++ index_Make/src/pages/navbar.jsx
@@ -96,14 +96,16 @@ const Navbar = () => {
               <DropdownMenuItem>
                 <Link
                   to={
-                    user.tipoUser === "empresa" ? "/minha-empresa" : "/meu-perfil"
+                    user.tipoUser === "empresa"
+                      ? "/minha-empresa"
+                      : "/meu-perfil"
                   }
                   className="w-full"
                 >
-                  {user.tipoUser === "cliente" ? "Minha empresa" : "Meu perfil"}
+                  {user.tipoUser === "empresa" ? "Minha empresa" : "Meu perfil"}
                 </Link>
               </DropdownMenuItem>
-              {user.tipoUser === "Empresa" && (
+              {user.tipoUser === "empresa" && (
                 <DropdownMenuItem>
                   <Link to="/agenda" className="w-full">
                     Agenda
